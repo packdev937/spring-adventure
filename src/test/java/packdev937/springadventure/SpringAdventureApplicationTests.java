c34b52147d14d89ef718b4b2eb33c0877d07fe20
Geonwoo Pack <packdev937@gmail.com>
2024-05-31 09:41:19 +0900

    initial commit

---
+++ src/test/java/packdev937/springadventure/SpringAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package packdev937.springadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
